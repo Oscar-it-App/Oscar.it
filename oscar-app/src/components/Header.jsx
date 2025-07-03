5695e49e0225cc4c9b22105cf77e801afc37dc1f
Oscar-it-App <e.c.propertyhomes@gmail.com>
2025-07-03 15:27:16 -0400

    Update Header.jsx

--- oscar-app/src/components/Header.jsx
+++ oscar-app/src/components/Header.jsx
@@ -18,11 +18,3 @@ const Header = () => {
 
 export default Header;
 
-// FILE: src/pages/Dashboard.jsx
-import React from 'react';
-
-const Dashboard = () => {
-  return <div className="p-6 text-lg">Welcome to the OSCAR dashboard!</div>;
-};
-
-export default Dashboard;
